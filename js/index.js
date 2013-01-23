1039402578db06e88346cf827cfbc457ce4d6d37
zodiac1111 <zodiac8765+github_admin@gmail.com>
2013-01-23 08:50:35 +0800

    首页

---
+++ js/index.js
@@ -0,0 +1,67 @@
+/**
+ * @author leepeigang
+ */
+$(document).ready(function() {
+	$("#btnSysParam").click(function() {
+		//$("#inter_page").html("");
+		$("#inter_page").addClass('load_bgpic_hight');
+		$.get("/um/syspara1.html", function(result) {
+			$("#inter_page").removeClass('load_bgpic_hight');
+			$("#inter_page").html(result);
+		});
+	});
+	$("#btnSioParam").click(function() {
+		$("#inter_page").html("");
+		$("#inter_page").addClass('load_bgpic_hight');
+		$.get("/um/compara1.html", function(result) {
+			$("#inter_page").removeClass('load_bgpic_hight');
+			$("#inter_page").html(result);
+		});
+	});
+	$("#btnNetParam").click(function() {
+		$("#inter_page").html("");
+		$("#inter_page").addClass('load_bgpic_hight');
+		$.get("/um/netpara1.html", function(result) {
+			$("#inter_page").removeClass('load_bgpic_hight');
+			$("#inter_page").html(result);
+		});
+	});
+	$("#btnMonParam").click(function() {
+		$("#inter_page").html("");
+		$("#inter_page").addClass('load_bgpic_hight');
+		$.get("/um/monpara1.html", function(result) {
+			$("#inter_page").removeClass('load_bgpic_hight');
+			$("#inter_page").html(result);
+		});
+	});
+		$("#btnMtrParam").click(function() {
+		$("#inter_page").html("");
+		$("#inter_page").addClass('load_bgpic_hight');
+		$.get("/um/meterpara1.html", function(result) {
+			$("#inter_page").removeClass('load_bgpic_hight');
+			$("#inter_page").html(result);
+		});
+	});
+		$("#btnSaveCycle").click(function() {
+		$("#inter_page").html("");
+		$("#inter_page").addClass('load_bgpic_hight');
+		$.get("/um/saveCycle.html", function(result) {
+			$("#inter_page").removeClass('load_bgpic_hight');
+			$("#inter_page").html(result);
+		});
+	});
+	$(function() {
+		$("#accordion").accordion({
+			heightStyle : "content"
+		});
+	});
+	$(function() {
+		$(".btn").button({
+			icons : {
+				primary : "ui-icon-power"
+			}
+		});
+	});
+	//起始动作,加载到系统参数页面
+	$("#btnSysParam").click();
+});
