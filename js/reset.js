9028e62827903d6ed036787b4b1bef46801b89ff
zodiac1111 <zodiac8765+github_admin@gmail.com>
2013-03-18 09:32:27 +0800

    服务器编译日期

--- js/reset.js
+++ js/reset.js
@@ -416,6 +416,7 @@ $(document).ready(function() {// 所有脚本都在文档加载完全后执行
 			$("#info_rtupara").html(oInfo.info_rtupara);
 			$("#info_wwwroot").html(oInfo.info_wwwroot);
 			$("#info_webbin").html(oInfo.info_webbin);
+			$("#build_time").html(oInfo.build_time);
 			//$("#procotol_text").removeClass("textarea_bgpic");
 			//$("#procotol_text").val(result);
 		});
