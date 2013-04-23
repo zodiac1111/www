ddf255738f90302876dd4d033cf40d833919ea87
zodiac1111 <zodiac8765+github_admin@gmail.com>
2013-04-23 17:04:00 +0800

    时间函数,完善

--- style/clock.js
+++ style/clock.js
@@ -1,8 +1,8 @@
-
+/*
 function Year_Month(){ powered by 25175.net
-    var now = new Date(); 
-    var yy = now.getYear(); 
-    var mm = now.getMonth(); 
+    var now = new Date();
+    var yy = now.getYear();
+    var mm = now.getMonth();
     var mmm=new Array();
     mmm[0]="January";
     mmm[1]="February ";
@@ -18,32 +18,33 @@ function Year_Month(){ powered by 25175.net
     mmm[11]="December";
     mm=mmm[mm];
     return(mm ); }
-function thisYear(){ 
-    var now = new Date(); 
-    var yy = now.getYear(); 
+function thisYear(){
+    var now = new Date();
+    var yy = now.getYear();
     return(yy ); }
-function Date_of_Today(){ 
-    var now = new Date(); 
+function Date_of_Today(){
+    var now = new Date();
     return(now.getDate() ); }
-function CurentTime(){ 
-    var now = new Date(); 
-    var hh = now.getHours(); 
-    var mm = now.getMinutes(); 
-    var ss = now.getTime() % 60000; 
-    ss = (ss - (ss % 1000)) / 1000; 
-    var clock = hh+':'; 
-    if (mm < 10) clock += '0'; 
-    clock += mm+':'; 
-    if (ss < 10) clock += '0'; 
-    clock += ss; 
-    return(clock); } 
-function refreshCalendarClock(){ 
-document.all.calendarClock1.innerHTML = Year_Month(); 
-document.all.calendarClock2.innerHTML = Date_of_Today(); 
-document.all.calendarClock3.innerHTML =thisYear(); 
+function CurentTime(){
+    var now = new Date();
+    var hh = now.getHours();
+    var mm = now.getMinutes();
+    var ss = now.getTime() % 60000;
+    ss = (ss - (ss % 1000)) / 1000;
+    var clock = hh+':';
+    if (mm < 10) clock += '0';
+    clock += mm+':';
+    if (ss < 10) clock += '0';
+    clock += ss;
+    return(clock); }
+function refreshCalendarClock(){
+document.all.calendarClock1.innerHTML = Year_Month();
+document.all.calendarClock2.innerHTML = Date_of_Today();
+document.all.calendarClock3.innerHTML =thisYear();
 document.all.calendarClock4.innerHTML = CurentTime(); }
 document.write('<font id="calendarClock1" > </font>&nbsp;');
 document.write('<font id="calendarClock2" > </font>,');
 document.write('<font id="calendarClock3" > </font>&nbsp;');
 document.write('<font id="calendarClock4" > </font>');
 setInterval('refreshCalendarClock()',1000);
+*/
